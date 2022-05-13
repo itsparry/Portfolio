37cacd91b5409f213123bedde824b357c6cd6db6
Parry Chan <parry_chan@hotmail.com>
2022-05-12 20:16:58 -0400

    header styling change

--- src/pages/Homepage/Header.jsx
+++ src/pages/Homepage/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <div className='header'>
     <h1 className='parry'>Hi! I'm Parry</h1>
     <div className='header-subtitle-container'>
-        <span className='header-s-1'>Product Designer + Front-end Developer</span>
+        <span className='pdfd'>Product Designer + Front-end Developer</span>
         <div className='lets-talk-container'>
-          <span className='header-s-2'>Seeking opportunities</span>
+          <span className='seekops'>Seeking opportunities</span>
           <a href='mailto:parry_chan@hotmail.com' target='_blank' className='header-s-3' rel="noreferrer">
             <div className='header-s-3' >
               <span className='lets-talk'>Lets talk!</span>
